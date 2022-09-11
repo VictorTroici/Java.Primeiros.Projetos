3985a93a5ca4d549d622c40efea6b94c84bfb3c0
Victor Camargo Troici <112210599+VictorTroici@users.noreply.github.com>
2022-09-11 14:52:31 -0300

    Add files via upload

---
+++ Impares.java
@@ -0,0 +1,12 @@
+
+public class Impares {
+    
+    public static void main(String [] args){
+    
+        for (int contador = 1; contador <=50; contador++){
+        
+        if(contador %2!=0) 
+            System.out.println("Numeros impares de 1 a 49 e: " + contador );           
+        }
+    } 
+}
---
+++ Pares.java
@@ -0,0 +1,16 @@
+
+public class Pares {
+    
+    public static void main(String [] args){
+    
+        int contador = 1;
+         
+        while  (contador <=50){
+        
+        if(contador %2==0) 
+            System.out.println("Numeros Pares de 2 a 50 e: " + contador ); 
+        contador = contador + 1;
+        }
+          
+    } 
+}
